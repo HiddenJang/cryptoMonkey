9ab359b2588952740fd837cbbfe3c51ee5ec0e24
Pavel <143640520+HiddenJang@users.noreply.github.com>
2024-02-22 18:24:49 +0400

    Update CryptoMonkeyGui.py
    del keys path

--- CryptoMonkeyGui.py
+++ CryptoMonkeyGui.py
@@ -54,7 +54,7 @@ class CryptoMonkeyGui(QMainWindow):
     infoUnfilledRowNumber = 0
     ordersUnfilledRowNumber = 0
     unfilledDiagRowNum = 0
-    keysPath = 'D:\\Pasha\\python\\projects\\.txt'  # путь к файлу с ключами от аккаунта пользователя
+    keysPath = 'D:\\python\\projects'  # путь к файлу с ключами от аккаунта пользователя
     openOrdersListPath = path + '\\open_orders.txt'  # путь к файлу с перечнем открытых ордеров
     imitationMode = False
 
